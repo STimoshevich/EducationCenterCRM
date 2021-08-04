21cfc8a23e05c747b9917989321309dd57d865ac
Timoshevich <inspectorcot@gmail.com>
2021-08-04 07:09:53 +0300

    Add validation for person model

--- EducationCenterCRM.DAL/Entities/Person.cs
+++ EducationCenterCRM.DAL/Entities/Person.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EducationCenterCRM.DAL.Entities.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +10,29 @@ namespace EducationCenterCRM.DAL.Entities
 {
     public abstract class Person
     {
+        [Required]
         public int Id { get; set; }
 
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Lenght should be less than 10")]
         public string Name { get; set; }
 
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Lenght should be less than 10")]
         public string Lastname { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [Phone]
         public string Phone { get; set; }
 
+        [Required]
+        [Age(18,80,"Age should be between 18 and 80")]
         public DateTime BirthDate { get; set; }
     }
 }
