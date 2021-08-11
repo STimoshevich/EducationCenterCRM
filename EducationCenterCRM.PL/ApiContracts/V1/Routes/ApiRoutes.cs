2bbaa474adc26ac426fb7714504d6de6255d2734
Timoshevich <inspectorcot@gmail.com>
2021-08-11 13:17:48 +0300

    Add api routes contract

---
+++ EducationCenterCRM.PL/ApiContracts/V1/Routes/ApiRoutes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenterCRM.PL.ApiContracts.V1.Routes
+{
+    public class ApiRoutes
+    {
+        public const string Varsion = "v1";
+        public const string Root = "api";
+        public const string Base = Root + "/" + Varsion;
+
+        public class Group
+        {
+            public const string GetAll = Base + "/groups";
+            public const string Create = Base + "/groups";
+            public const string Update = Base + "/groups";
+            public const string Get = Base + "/groups/{id}";
+            public const string Delete = Base + "/groups/{id}";
+
+        }
+
+    }
+}
