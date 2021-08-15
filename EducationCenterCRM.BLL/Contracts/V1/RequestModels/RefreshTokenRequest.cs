58413dfd3d5da417e76f853e6b01a7f7a9b335e8
Timoshevich <inspectorcot@gmail.com>
2021-08-15 22:13:42 +0300

    Add request models

---
+++ EducationCenterCRM.BLL/Contracts/V1/RequestModels/RefreshTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenterCRM.BLL.Contracts.V1.RequestModels
+{
+    public class RefreshTokenRequest
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
