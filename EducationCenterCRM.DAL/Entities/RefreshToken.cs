503a9f8e2cc7e5dc97eb15027343a43caf4efcd4
Timoshevich <inspectorcot@gmail.com>
2021-08-15 22:17:38 +0300

    add refresh token

---
+++ EducationCenterCRM.DAL/Entities/RefreshToken.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenterCRM.DAL.Entities
+{
+    public class RefreshToken
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Token { get; set; }
+        public string JwtId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public bool Used { get; set; }
+        public bool Invalidated { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
+    }
+}
