216dd9f10c12f230c9f36fc7686dd32ede34be80
Timoshevich <inspectorcot@gmail.com>
2021-08-22 22:08:26 +0300

    Add validators

---
+++ EducationCenterCRM.BLL/Contracts/V1/Validators/CourseRequestValidator.cs
@@ -0,0 +1,36 @@
+﻿using EducationCenterCRM.BLL.Contracts.V1.RequestModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenterCRM.BLL.Contracts.V1.Validators
+{
+    public class CourseRequestValidator : AbstractValidator<CourseRequest>
+    {
+
+        public CourseRequestValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(300);
+
+            RuleFor(X=>X.Program)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(500);
+
+
+            RuleFor(X => X.TopicId)
+               .GreaterThan(0);
+
+        }
+    }
+}
