6f3d0a9898885a68e8573d2bb5f0e36d8c3a1d4e
Timoshevich <inspectorcot@gmail.com>
2021-08-22 22:32:25 +0300

    HW Add Models for appsettings binding

--- EducationCenterCRM.BLL/Options/IdentitySettings.cs
+++ EducationCenterCRM.BLL/Options/IdentitySettings.cs
@@ -1,14 +1,41 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace EducationCenterCRM.BLL.Options
 {
     public class IdentitySettings
     {
-       public List<string> AdminEmails { get; set; } = new List<string>();
-        public List<string> ManagerEmails { get; set; } = new List<string>();
+        private readonly IConfiguration configuration;
+        private IdentitySettingsValues values = null;
+
+        public IdentitySettings(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IdentitySettingsValues Get()
+        {
+           if(values is null)
+            {
+
+                values = new IdentitySettingsValues();
+                configuration.Bind(nameof(IdentitySettings), values);
+            }
+               
+            return values;
+        }
+
+
+
+        public class IdentitySettingsValues
+        {
+           
+            public List<string> AdminsEmails { get; set; } = new List<string>();
+            public List<string> ManagersEmails { get; set; } = new List<string>();
+        }
     }
+
+    
 }
